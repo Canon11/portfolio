daa2c198d5f57e0d4f4c53473c293a5e841af4d4
kazuma murata <mrt_014_kzm@yahoo.co.jp>
2019-02-21 01:28:24 +0900

    edit timeline and images

--- src/components/PhotoGallery.vue
+++ src/components/PhotoGallery.vue
@@ -8,9 +8,10 @@
 export default {
     data: () => ({
         images: [
-            'https://d1f5hsy4d47upe.cloudfront.net/9b/9b534625bee11de6210aa94dbd83ff24_w.jpg',
-            'https://d1f5hsy4d47upe.cloudfront.net/36/3683fcd16a4c79e00b4339c951fcb5f4_w.jpg',
-            'https://d1f5hsy4d47upe.cloudfront.net/27/2772d37f06e7d34c4204d42c50d35f57_w.jpg'
+            require('../assets/img1.jpg'),
+            require('../assets/img2.jpg'),
+            require('../assets/img3.jpg'),
+            require('../assets/img4.jpg'),
         ]
     })
 }
--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -18,33 +18,27 @@ export default {
     data: () => ({
         years: [{
                 color: 'cyan',
-                year: '1960',
-                title: 'text1',
-                description: 'description 1aweofijaweofiwaofijawoefijaowifjawe'
+                year: '2013',
+                title: '筑波大学入学',
+                description: '筑波大に入学し、情報学を学び始める。Javaを書きながらも理解に苦しみ、プログラミングに苦手意識を持ち始める。サークルはストリートダンス。'
             },
             {
                 color: 'green',
-                year: '1970',
-                title: 'text2',
-                description: 'description 2'
+                year: '2015',
+                title: 'Web開発にのめり込む',
+                description: 'PythonでWeb開発を初め、Webプログラミングの楽しさを知る。プログラミングが好きになる。院進するつもりだったが、大学卒業後の就職をここで決める。'
             },
             {
                 color: 'pink',
-                year: '1980',
-                title: 'text3',
-                description: 'description 3'
+                year: '2017',
+                title: '大学卒業・新卒入社',
+                description: '大学を卒業し、Web系ベンチャーに入社。ソーシャルゲームのサーバサイドエンジニアとしてバックエンド開発全般を担当するようになる。新規開発に苦しむ。'
             },
             {
                 color: 'amber',
-                year: '1990',
-                title: 'text4',
-                description: 'description 4'
-            },
-            {
-                color: 'orange',
-                year: '2000',
-                title: 'text5',
-                description: 'description 5'
+                year: '2018',
+                title: '退職・起業',
+                description: '新卒で入った会社を退職し、大学時代の先輩とその友達と株式会社を起業。受注開発やシステム開発をしながらも、収益アップに苦しむ（現在進行形）'
             }
         ]
     })
