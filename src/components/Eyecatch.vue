8473da629c83828ed6167cd3fb07fa94dc2b8a64
Canon11 <mrt014kzm@gmail.com>
2019-03-12 14:26:07 +0900

    edit typing script in eyecatch

--- src/components/Eyecatch.vue
+++ src/components/Eyecatch.vue
@@ -1,15 +1,16 @@
 <template>
   <v-layout align-center justify-center>
     <v-flex sm3>
-      <div class="left-block">
+      <div class="left-block pt-2">
         <ul>
           <!-- 1階層目のフォルダをループ -->
           <li v-for="(rootFolder, i) in rootFolders" :key="i">
             <v-icon v-if="rootFolder.folders.length > 0" color="orange">{{ rootFolder.icon }}</v-icon>
             <v-icon v-else dark>{{ rootFolder.icon }}</v-icon>
             <span
+              class="ml-1"
               :class="{ main_filename: rootFolder.folders.length > 0}"
-            >&nbsp;{{ rootFolder.name }}</span>
+            >{{ rootFolder.name }}</span>
 
             <ul v-if="rootFolder.folders.length > 0">
               <!-- 2階層目のフォルダをループ。この層のみ、別ページへのリンクが存在する。 -->
@@ -20,8 +21,9 @@
                       <v-icon v-if="folder.files.length > 0" color="orange">{{ folder.icon }}</v-icon>
                       <v-icon v-else dark>{{ folder.icon }}</v-icon>
                       <span
+                        class="ml-1"
                         :class="{ main_filename: folder.files.length > 0 }"
-                      >&nbsp;{{ folder.name }}</span>
+                      >{{ folder.name }}</span>
                     </a>
                   </template>
                   <span>Open Link</span>
@@ -32,7 +34,7 @@
                   <li v-for="(file, k) in folder.files" :key="k">
                     <v-icon v-if="file.is_main" color="orange">{{ file.icon }}</v-icon>
                     <v-icon v-else dark>{{ file.icon }}</v-icon>
-                    <span :class="{ main_filename: file.is_main }">&nbsp;{{ file.name }}</span>
+                    <span class="ml-1" :class="{ main_filename: file.is_main }">{{ file.name }}</span>
                   </li>
                 </ul>
               </li>
@@ -42,7 +44,7 @@
       </div>
     </v-flex>
     <v-flex sm9>
-      <div class="right-block">
+      <div class="right-block pl-3">
         <div id="ityped" class="display-3 font-weight-light"></div>
       </div>
     </v-flex>
@@ -54,7 +56,6 @@
   background-color: gray;
   color: white;
   height: 100vh;
-  padding-top: 2vh;
 }
 .left-block ul {
   list-style: none;
@@ -85,12 +86,15 @@
   background-color: black;
   color: white;
   height: 100vh;
-  padding-left: 2%;
+}
+#ityped {
+  white-space: pre;
 }
 </style>
 
 <script>
 import { init } from "ityped";
+import Vue from "vue";
 import "@mdi/font/css/materialdesignicons.css";
 
 export default {
@@ -174,18 +178,32 @@ export default {
     // 右ブロックのタイピング設定
     init("#ityped", {
       strings: [
-        "def sayThanks():",
-        "print(\"Hello Muratty's Page! Thank's to visit!!\")",
-        'print("This is my profiles.")',
-        'print("Please scroll down.")'
+        "$ emacs welcome.py",
+        "def main():\n" +
+          '\tprint("Hello Muratty\'s Page!")\n' +
+          '\tprint("This is my portfolio.")\n' +
+          '\tprint("Please scroll down.")\n\n' +
+          'if __name__ == "__main__":\n' +
+          "\tmain()",
+        "$ python welcome.py"
       ],
-      typeSpeed: 55, // 表示する時のスピード
-      backSpeed: 30, // 戻る時のスピード
-      startDelay: 300, // スタート時の遅延時間
-      backDelay: 500, // 戻る時の遅延時間
+      typeSpeed: 35, // 表示する時のスピード
+      backSpeed: 0.5, // 戻る時のスピード
+      startDelay: 500, // スタート時の遅延時間
+      backDelay: 600, // 戻る時の遅延時間
       loop: false, // ループの有無
-      showCursor: true, // カーソル表示の有無
-      cursorChar: "|" // カーソルの形状
+      onFinished: function() {
+        // ある程度スクロールしているときは、強制的にスクロールさせない
+        if (window.pageYOffset > 300) {
+          return;
+        }
+        // アニメーションが終わったら、profile部分にスクロールさせる
+        Vue.prototype.$vuetify.goTo("#profile", {
+          duration: 1500,
+          offset: 10,
+          easing: "easeInOutCubic"
+        });
+      }
     });
   }
 };
