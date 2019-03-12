796fca8fac6f0ffd7f17222b02006f06015e97b4
Canon11 <mrt014kzm@gmail.com>
2019-03-12 13:42:05 +0900

    add explorer to eyecatch

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,57 +1,54 @@
 <template>
-<div class="my-5">
+  <div class="my-5">
     <div class="display-2 font-weight-regular font-italic text-sm-center">New Blog</div>
     <v-layout row justify-space-around class="mt-4">
-        <v-flex sm3 v-for="(blog, i) in blogs" :key="i">
-            <v-card>
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px">
-                </v-img>
+      <v-flex sm3 v-for="(blog, i) in blogs" :key="i">
+        <v-card>
+          <v-img :src="blog.image" height="200px"></v-img>
 
-                <v-card-title primary-title>
-                    <div>
-                        <div class="headline">{{ blog.title }}</div>
-                        <span class="grey--text">1,000 miles of wonder</span>
-                    </div>
-                </v-card-title>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">{{ blog.title }}</div>
+            </div>
+          </v-card-title>
 
-                <v-card-actions>
-                    <v-btn flat>Share</v-btn>
-                    <v-btn flat color="purple">Explore</v-btn>
-                    <v-spacer></v-spacer>
-                    <v-btn icon @click="blog.show = !blog.show">
-                        <v-icon>{{ blog.show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-                    </v-btn>
-                </v-card-actions>
-
-                <v-slide-y-transition>
-                    <v-card-text v-show="blog.show">{{ blog.description }}</v-card-text>
-                </v-slide-y-transition>
-            </v-card>
-        </v-flex>
+          <v-card-actions>
+            <v-btn flat color="purple" :href="blog.source" target="_blank">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-layout>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        show: false,
-        blogs: [{
-                title: 'blog1',
-                description: 'desc1',
-                show: false
-            },
-            {
-                title: 'blog2',
-                description: 'desc2',
-                show: false
-            },
-            {
-                title: 'blog3',
-                description: 'desc3',
-                show: false
-            }
-        ]
-    })
-}
+  data: () => ({
+    show: false,
+    blogs: [
+      {
+        title:
+          "AWSソリューションアーキテクトアソシエイトの勉強範囲まとめ(2019年3月)",
+        description: "desc1",
+        image:
+          "https://murattyblog.com/wp-content/uploads/2019/03/eyecatch.039.png",
+        source: "https://murattyblog.com/aws_saa_study"
+      },
+      {
+        title: "Atom卒業!? MacでVSCodeを使ってみた3つの感想",
+        description: "desc1",
+        image:
+          "https://murattyblog.com/wp-content/uploads/2019/02/eyecatch.038.png",
+        source: "https://murattyblog.com/vscode_install"
+      },
+      {
+        title: "AWSソリューションアーキテクトアソシエイトに合格しました！",
+        description: "desc1",
+        image:
+          "https://murattyblog.com/wp-content/uploads/2019/02/eyecatch.037.png",
+        source: "https://murattyblog.com/aws_saa"
+      }
+    ]
+  })
+};
 </script>
--- src/components/Eyecatch.vue
+++ src/components/Eyecatch.vue
@@ -1,7 +1,48 @@
 <template>
   <v-layout align-center justify-center>
-    <v-flex sm12>
-      <div class="ityped-block" style="height:100vh">
+    <v-flex sm3>
+      <div class="left-block">
+        <ul>
+          <!-- 1階層目のフォルダをループ -->
+          <li v-for="(rootFolder, i) in rootFolders" :key="i">
+            <v-icon v-if="rootFolder.folders.length > 0" color="orange">{{ rootFolder.icon }}</v-icon>
+            <v-icon v-else dark>{{ rootFolder.icon }}</v-icon>
+            <span
+              :class="{ main_filename: rootFolder.folders.length > 0}"
+            >&nbsp;{{ rootFolder.name }}</span>
+
+            <ul v-if="rootFolder.folders.length > 0">
+              <!-- 2階層目のフォルダをループ。この層のみ、別ページへのリンクが存在する。 -->
+              <li v-for="(folder, j) in rootFolder.folders" :key="j">
+                <v-tooltip bottom open-delay="50">
+                  <template v-slot:activator="{ on }">
+                    <a :href="folder.source" target="_blank" v-on="on">
+                      <v-icon v-if="folder.files.length > 0" color="orange">{{ folder.icon }}</v-icon>
+                      <v-icon v-else dark>{{ folder.icon }}</v-icon>
+                      <span
+                        :class="{ main_filename: folder.files.length > 0 }"
+                      >&nbsp;{{ folder.name }}</span>
+                    </a>
+                  </template>
+                  <span>Open Link</span>
+                </v-tooltip>
+
+                <ul v-if="folder.files.length > 0">
+                  <!-- 3階層目のフォルダをループ -->
+                  <li v-for="(file, k) in folder.files" :key="k">
+                    <v-icon v-if="file.is_main" color="orange">{{ file.icon }}</v-icon>
+                    <v-icon v-else dark>{{ file.icon }}</v-icon>
+                    <span :class="{ main_filename: file.is_main }">&nbsp;{{ file.name }}</span>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </v-flex>
+    <v-flex sm9>
+      <div class="right-block">
         <div id="ityped" class="display-3 font-weight-light"></div>
       </div>
     </v-flex>
@@ -9,25 +50,128 @@
 </template>
 
 <style scoped>
-.ityped-block {
+.left-block {
+  background-color: gray;
+  color: white;
+  height: 100vh;
+  padding-top: 2vh;
+}
+.left-block ul {
+  list-style: none;
+}
+.left-block a {
+  text-decoration: none;
+  color: inherit;
+  display: inline-block;
+}
+.left-block a:hover {
+  -webkit-animation: zoom 0.3s;
+  animation: zoom 0.3s;
+}
+@-webkit-keyframes zoom {
+  50% {
+    -webkit-transform: scale(1.1);
+  }
+}
+@keyframes zoom {
+  50% {
+    transform: scale(1.1);
+  }
+}
+.main_filename {
+  color: orange;
+}
+.right-block {
   background-color: black;
   color: white;
+  height: 100vh;
+  padding-left: 2%;
 }
 </style>
 
-
 <script>
 import { init } from "ityped";
+import "@mdi/font/css/materialdesignicons.css";
 
 export default {
   data: () => ({
-    scrollOpt: {
-      duration: 1500,
-      offset: 10,
-      easing: "easeInOutCubic"
-    }
+    // 左ブロックに表示するファイル一覧
+    rootFolders: [
+      {
+        name: "Projects",
+        icon: "mdi-folder",
+        folders: [
+          {
+            name: "Portfolio",
+            icon: "mdi-folder-star",
+            source: "https://github.com/Canon11/portfolio",
+            files: [
+              {
+                name: "welcome.py",
+                icon: "mdi-language-python",
+                is_main: true // is_mainをtrueにしておくと、ファイル名がオレンジでハイライトされる
+              },
+              {
+                name: "profile.py",
+                icon: "mdi-language-python",
+                is_main: false
+              },
+              {
+                name: "timeline.py",
+                icon: "mdi-language-python",
+                is_main: false
+              },
+              {
+                name: "new_blogs.py",
+                icon: "mdi-language-python",
+                is_main: false
+              },
+              {
+                name: "README.md",
+                icon: "mdi-information-outline",
+                is_main: false
+              },
+              {
+                name: ".gitignore",
+                icon: "mdi-git",
+                is_main: false
+              }
+            ]
+          },
+          {
+            name: "dj-cref",
+            icon: "mdi-folder-star",
+            source: "https://github.com/Canon11/dj-cref",
+            files: []
+          },
+          {
+            name: "TweetAutoFollow",
+            icon: "mdi-folder-star",
+            source: "https://github.com/Canon11/TweetAutoFollow",
+            files: []
+          },
+          {
+            name: "YAlert",
+            icon: "mdi-folder-star",
+            source: "https://github.com/Canon11/YAlert",
+            files: []
+          }
+        ]
+      },
+      {
+        name: "Documents",
+        icon: "mdi-folder",
+        folders: []
+      },
+      {
+        name: "Downloads",
+        icon: "mdi-folder",
+        folders: []
+      }
+    ]
   }),
   mounted: function() {
+    // 右ブロックのタイピング設定
     init("#ityped", {
       strings: [
         "def sayThanks():",
@@ -41,10 +185,7 @@ export default {
       backDelay: 500, // 戻る時の遅延時間
       loop: false, // ループの有無
       showCursor: true, // カーソル表示の有無
-      cursorChar: "|", // カーソルの形状
-      onFinished: function() {
-        this.$vuetify.goTo("#profile", this.scrollOpt);
-      }
+      cursorChar: "|" // カーソルの形状
     });
   }
 };
